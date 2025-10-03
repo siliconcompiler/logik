0d24943b44583713aa7a037ec2dcd71030628c6d
Fred Tombs <fred@zeroasic.com>
2025-10-03 17:03:47 -0400

    trying to make CI pass

--- tests/examples/test_files.py
+++ tests/examples/test_files.py
@@ -1,19 +1,13 @@
 # Copyright 2024 Zero ASIC Corporation
 # Licensed under the MIT License (see LICENSE for details)
 
-import pytest
 import siliconcompiler
 
 from logik.demo.z1000 import z1000
 
 
-@pytest.mark.parametrize(
-    "part",
-    [
-        z1000,
-    ])
 def test_file_paths(part):
     project = siliconcompiler.FPGA("test")
-    project.set_fpga(part())
+    project.set_fpga(z1000())
 
     assert project.check_filepaths()
