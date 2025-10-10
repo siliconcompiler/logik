337eee49c5519e4968b0e6a37a8a8b4f3ff34600
Fred Tombs <fred@zeroasic.com>
2025-10-10 14:13:38 -0400

    picorv32 example update

--- examples/picorv32/picorv32.py
+++ examples/picorv32/picorv32.py
@@ -1,45 +1,38 @@
 #!/usr/bin/env python3
 
-# This is the logik run script for demonstrating RTL-to-bitstream
-# with Alex Forencich's 1G Ethernet MAC
-
 import siliconcompiler
 from logik.flows.logik_flow import LogikFlow
-
 
 from logik.z1062_local_cad import z1062  # Temporary
 
 
 def build():
-    design = siliconcompiler.Design('picorv32')
+    module_name = 'picorv32'
+    design = siliconcompiler.Design(module_name)
 
-    # Define source files from verilog-ethernet repo
-
-    # First we need to register the verilog-ethernet repo
-    # as a package
+    # Fetch picorv32 from the logikbench repo
+    # Silicon Compiler will download and cache the files for us
     design.set_dataroot(
         'picorv32-logikbench',
         'git+https://github.com/zeroasiccorp/logikbench.git',
         'db866c536340c071c563a063c9406888070dfbda')
 
-    # Then we can pull in the specific RTL we need from that
-    # repository -- Silicon Compiler will download and cache the files
-    # for us
     with design.active_dataroot('picorv32-logikbench'):
-        design.add_file(f'logikbench/blocks/picorv32/rtl/picorv32.v', fileset='rtl')
-        design.set_topmodule("picorv32", fileset="rtl")
+        design.add_file(f'logikbench/blocks/{module_name}/rtl/{module_name}.v', fileset='rtl')
+        design.set_topmodule(module_name, fileset="rtl")
 
     design.set_dataroot('constraints', __file__)
     with design.active_dataroot('constraints'):
         # Add timing constraints
-        design.add_file('picorv32.sdc', fileset='sdc')
+        design.add_file(f'{module_name}.sdc', fileset='sdc')
 
         # Define pin constraints
-        design.add_file("constraints/z1062/picorv32.pcf",
+        design.add_file(f"constraints/z1062/{module_name}.pcf",
             fileset='pcf')
 
     project = siliconcompiler.FPGA(design)
 
+    # add design files to the project
     project.add_fileset('rtl')
     project.add_fileset('sdc')
     project.add_fileset('pcf')
