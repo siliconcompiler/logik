83ced1b6797d4aa0ca94658e44380b6fa52b5552
Fred Tombs <fred@zeroasic.com>
2025-10-18 13:26:08 -0400

    Remove test restrictions

--- tests/examples/test_files.py
+++ tests/examples/test_files.py
@@ -1,14 +1,11 @@
 # Copyright 2024 Zero ASIC Corporation
 # Licensed under the MIT License (see LICENSE for details)
 
-import pytest
-
 import siliconcompiler
 
 from logik.demo.z1000 import z1000
 
 
-@pytest.mark.skip(reason="need new cad tarballs from logiklib")
 def test_file_paths():
     project = siliconcompiler.FPGA("test")
     project.set_fpga(z1000())
