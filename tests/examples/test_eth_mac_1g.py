236be7d339abeff0f0af308ba7f8c5eaf3bae72f
Fred Tombs <fred@zeroasic.com>
2026-04-07 10:42:49 -0400

    Increase timeout to account for slow github runners

--- tests/examples/test_eth_mac_1g.py
+++ tests/examples/test_eth_mac_1g.py
@@ -7,7 +7,7 @@ import subprocess
 import pytest
 
 
-@pytest.mark.timeout(360)
+@pytest.mark.timeout(720)
 def test_py(setup_example_test, monkeypatch):
     eth_mac_1g_dir = setup_example_test('eth_mac_1g')
 
@@ -17,7 +17,7 @@ def test_py(setup_example_test, monkeypatch):
     eth_mac_1g.build()
 
 
-@pytest.mark.timeout(360)
+@pytest.mark.timeout(720)
 def test_cli(setup_example_test):
     eth_mac_1g_dir = setup_example_test('eth_mac_1g')
 
